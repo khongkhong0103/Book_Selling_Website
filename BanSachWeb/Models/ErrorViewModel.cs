c4cd2d94f522be90f10cd25dd87be8e7e44934aa
Trong Tam <trongtam068@gmail.com>
2023-10-09 21:17:12 +0700

    ...

--- BanSachWeb/Models/ErrorViewModel.cs
+++ BanSachWeb/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace BanSachWeb.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
